b7d0ff6b72f8afea298b30195a0dcb4f7aedfd88
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-01-09 17:12:09 +0200

    footer edited

--- src/Components/Newsletter/Newsletter.jsx
+++ src/Components/Newsletter/Newsletter.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import './Newsletter.css'
+import React from "react";
+import "./Newsletter.css";
 
 export const Newsletter = () => {
   return (
-    <div className='newsletter'>
-        <h1>Get Exclusive Offers on Your Email</h1>
-        <p>Subscribe to our newsletter and stay updated</p>
+    <div className="newsletter">
+      <h1>Get Exclusive Offers on Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type="email" placeholder="Your Email id" />
+        <button>Subscribe</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
